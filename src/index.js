56bd57a6105bfdd0f373161d72a5f99b588dfe0e
prudence <prudence.jerono@student.moringaschool.com>
2023-04-24 00:30:09 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -3,3 +3,19 @@ import ReactDOM from "react-dom";
 import Counter from "./components/Counter";
 
 ReactDOM.render(<Counter />, document.getElementById("root"));
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
